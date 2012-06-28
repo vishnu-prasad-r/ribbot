df304bec4f985165e588f67f33f5960a41211943
Malo Skrylevo <3aHyga@gmail.com>
2012-06-28 14:43:38 +0400

    Added the school field to the user model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if !ENV['FACEBOOK_KEY'].blank? && !ENV['FACEBOOK_SECRET'].blank?
     provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-	:scope => 'email,user_website'
+	:scope => 'email,user_education_history,user_website'
   end
 end
 
